f40ea9071c08d451fa81210146bdc8264fe5ed78
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-07 12:00:00 +0100

    php docker entrypoint: install composer

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 set -e
+
+# Run composer install if composer.json exists
+if [ -f "composer.json" ]; then
+    echo "Installing dependencies..."
+    composer install --no-interaction --optimize-autoloader
+fi
