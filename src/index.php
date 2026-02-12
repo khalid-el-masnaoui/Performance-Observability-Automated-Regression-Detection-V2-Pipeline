0130a086bf4dc2089f755f7c7192a15af63b473e
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-12 12:00:00 +0100

    php app entry point: redis connection

--- src/index.php
+++ src/index.php
@@ -4,3 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\APC;
+
+// ---------------------------
+// Redis (for SPX trigger)
+// ---------------------------
+$redis = new Redis();
+$redis->connect('redis');
