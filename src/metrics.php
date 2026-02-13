f7d5bbb34a43ea8f168ee4fab915ea5a0715e7ce
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-13 12:00:00 +0100

    render metrics

--- src/metrics.php
+++ src/metrics.php
@@ -1,3 +1,18 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
+use Prometheus\CollectorRegistry;
+use Prometheus\RenderTextFormat;
+use Prometheus\Storage\APC;
+
+// Same storage as index.php
+$registry = new CollectorRegistry(new APC());
+
+$renderer = new RenderTextFormat();
+
+header('Content-Type: ' . RenderTextFormat::MIME_TYPE);
+
+echo $renderer->render(
+    $registry->getMetricFamilySamples()
+);
