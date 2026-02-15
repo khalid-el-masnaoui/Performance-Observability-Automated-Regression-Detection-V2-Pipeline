cee7d45dd41805b1d1a5815b6695fc85648cbf96
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-15 12:00:00 +0000

    alertmanager entrypoint

---
+++ alertmanager/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+# Replace placeholders with environment variable values in the config file
+sed -i "s|\${REGRESSION_SERVICE_URL}|${REGRESSION_SERVICE_URL}|g" /etc/alertmanager/alertmanager.yml
