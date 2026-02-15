45afccdffac7b87aeba6e304540bc1aa0b535a40
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-15 12:00:00 +0000

    alertmanager entrypoint: start service

--- alertmanager/entrypoint.sh
+++ alertmanager/entrypoint.sh
@@ -3,3 +3,6 @@ set -e
 
 # Replace placeholders with environment variable values in the config file
 sed -i "s|\${REGRESSION_SERVICE_URL}|${REGRESSION_SERVICE_URL}|g" /etc/alertmanager/alertmanager.yml
+
+# Start Alertmanager
+exec alertmanager --config.file=/etc/alertmanager/alertmanager.yml
