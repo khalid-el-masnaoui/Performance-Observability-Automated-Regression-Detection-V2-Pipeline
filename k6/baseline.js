a1304d0f135c5a8856733b75de6066a2305c768b
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-16 12:00:00 +0000

    k6 baseline test

--- k6/baseline.js
+++ k6/baseline.js
@@ -5,3 +5,18 @@ export const options = {
   vus: 10,
   duration: '30s',
 };
+
+const NGINX_URL = __ENV.NGINX_URL || "http://nginx";
+
+const routes = [
+  '/',
+  '/api/users',
+];
+
+export default function () {
+  routes.forEach(route => {
+    http.get(`${NGINX_URL}${route}`);
+  });
+
+  sleep(1);
+}
