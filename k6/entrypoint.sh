514429fd96ed229052fb85d4ea0905342899af0f
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-17 12:00:00 +0000

    k6 load testing entrypoint: define routes

--- k6/entrypoint.sh
+++ k6/entrypoint.sh
@@ -3,3 +3,9 @@
 NGINX_URL="${NGINX_URL:-'http://nginx'}"
 PROM_URL="${PROM_URL:-'http://prometheus:9090'}"
 REGRESSION_URL="${REGRESSION_SERVICE_URL:-'http://regression-service:8090'}"
+
+
+ROUTES=(
+  "/"
+  "/api/users"
+)
