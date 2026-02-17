5bf9f5f072037ea63b2ceae38e33c3885a6b9fd0
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-17 12:00:00 +0000

    k6 load testing entrypoint

---
+++ k6/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NGINX_URL="${NGINX_URL:-'http://nginx'}"
+PROM_URL="${PROM_URL:-'http://prometheus:9090'}"
+REGRESSION_URL="${REGRESSION_SERVICE_URL:-'http://regression-service:8090'}"
