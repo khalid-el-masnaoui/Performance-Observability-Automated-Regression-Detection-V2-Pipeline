68bacfef19fdaf6ebe17a45202f9a6830a0ef2cd
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-17 12:00:00 +0000

    k6 slow traffic test configs

--- k6/ingest_slow_requests.js
+++ k6/ingest_slow_requests.js
@@ -1,2 +1,14 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+
+const NGINX_URL = __ENV.NGINX_URL || "http://nginx";
+
+export const options = {
+  scenarios: {
+    steady_load: {
+      executor: 'constant-vus',
+      vus: 5,
+      duration: '2m',
+    },
+  },
+};
