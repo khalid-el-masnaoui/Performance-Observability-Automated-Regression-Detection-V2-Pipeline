f9c0638fc336f91f9314a54457aacee0836ea3b9
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-23 12:00:00 +0000

    local testing: store baseline script --> collecting metrics : throughput

--- testing/scripts/store-baseline.sh
+++ testing/scripts/store-baseline.sh
@@ -101,3 +101,15 @@ for route in "${ROUTES[@]}"; do
   MAX_LATENCY=$(curl -sG "$PROM_URL/api/v1/query" \
     --data-urlencode "query=$MAX_QUERY" \
     | jq -r '.data.result[0].value[1]')
+
+  # ---------------------------------------------------
+  # THROUGHPUT
+  # ---------------------------------------------------
+
+  THROUGHPUT_RPS_QUERY="
+    sum(rate(app_request_duration_seconds_count{route=\"$route\"}[1m]))
+  "
+
+  THROUGHPUT_RPS=$(curl -sG "$PROM_URL/api/v1/query" \
+    --data-urlencode "query=$THROUGHPUT_RPS_QUERY" \
+    | jq -r '.data.result[0].value[1]')
