8d022fa9d9280234f471b25577406d7443178578
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-02-27 12:00:00 +0000

    report service flask app

---
+++ report-service/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Image,
+    Table,
+    TableStyle
+)
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter, A3
+import os
+import datetime
+import json
+
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+app = Flask(__name__)
