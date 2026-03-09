df820030044f686a6d8f666f85a339b3fc424e4c
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-03-09 12:00:00 +0000

    regression service: import all needed dependecnies

--- regression-service/app.py
+++ regression-service/app.py
@@ -1,1 +1,21 @@
 from flask import Flask, request, jsonify
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Image,
+    Table,
+    TableStyle
+)
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib.pagesizes import letter, A3
+import os
+import datetime
+import json
+
+import matplotlib
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import pandas as pd
