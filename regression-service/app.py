947a8efa60fec23b0ccccc0dbc590336fbc58720
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-03-10 12:00:00 +0000

    regression service: redis connection

--- regression-service/app.py
+++ regression-service/app.py
@@ -12,3 +12,7 @@ REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 PROMETHEUS_URL = os.getenv("PROMETHEUS_URL", "http://prometheus:9090")
 REPORT_URL = os.getenv("REPORT_URL", "http://report-service:5000")
 SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")
+
+r = redis.Redis(host=REDIS_HOST, port=6379, decode_responses=True)
+
+print ("App started", flush=True)
