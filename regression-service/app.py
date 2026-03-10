bd55a0fed72bc31390c831a2bb96c195e3497aa7
khalid-masnaoui <elmasnaoui.khalid1@gmail.com>
2026-03-10 12:00:00 +0000

    regression service: configs

--- regression-service/app.py
+++ regression-service/app.py
@@ -6,3 +6,9 @@ import redis
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+# config (use env vars in real setup)
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+PROMETHEUS_URL = os.getenv("PROMETHEUS_URL", "http://prometheus:9090")
+REPORT_URL = os.getenv("REPORT_URL", "http://report-service:5000")
+SLACK_WEBHOOK = os.getenv("SLACK_WEBHOOK")
